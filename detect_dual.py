09b2e31bc6a2517a5592936d3b540d354f42c8ee
yingfengxue <yingfengxue1209@gmail.com>
2025-06-23 12:14:28 +0800

    Update detect_dual.py

--- detect_dual.py
+++ detect_dual.py
@@ -26,7 +26,7 @@ def run(
         source=ROOT / 'data/images',  # file/dir/URL/glob/screen/0(webcam)
         data=ROOT / 'data/multiplane.yaml',  # dataset.yaml path
         imgsz=(640, 640),  # inference size (height, width)
-        conf_thres=0.25,  # confidence threshold
+        conf_thres=0.001,  # confidence threshold
         iou_thres=0.45,  # NMS IOU threshold
         max_det=1000,  # maximum detections per image
         device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
